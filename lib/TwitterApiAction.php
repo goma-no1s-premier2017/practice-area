d0128b02c399130831b1cf5e6f164787cb6468f8
no1s-goma <application.goto.2020@gmail.com>
2018-04-29 21:03:14 +0900

    [update]画像取得処理をTwitterApiActionクラスへ移行

--- lib/TwitterApiAction.php
+++ lib/TwitterApiAction.php
@@ -11,6 +11,10 @@ class TwitterApiAction
     protected $accessToken;
     protected $accessTokenSecret;
 
+    // twitter parameters
+    protected $option_params;
+    protected $now;
+
     /**
      * コンストラクタ
      *
@@ -25,6 +29,8 @@ class TwitterApiAction
         $this->consumerSecret    = $consumerSecret;
         $this->accessToken       = $accessToken;
         $this->accessTokenSecret = $accessTokenSecret;
+
+        $this->now = date("Ymd_his");
     }
 
     /**
@@ -99,5 +105,92 @@ class TwitterApiAction
         $params['oauth_signature'] = base64_encode(hash_hmac('sha1', $signature_data, $signature_key, true));
 
         return http_build_query($params, '', ',');
+    }
+
+    /**
+     * 必要な場合、事前にパラメータをセットする
+     *
+     * @param array $option_params
+     * @return TwitterApiAction
+     */
+    public function PresetOptions($option_params)
+    {
+        if (isset($option_params) && is_array($option_params)) {
+            $this->option_params = $option_params;
+        }
+
+        return $this;
+    }
+
+    /**
+     * 一意のツイート画像を10件保存する
+     *
+     * @param array $aryKeyCheck
+     * @param number $img_count
+     * @param string $max_id
+     * @param number $request_count
+     * @return boolean
+     */
+    public function SaveUniqueTweetImages($aryKeyCheck = [], $img_count = 1, $max_id = "", $request_count = 1)
+    {
+        if ($request_count > MAX_REQUEST_COUNT) {
+            return false;
+        }
+
+        $option_params = $this->option_params;
+
+        if ($max_id !== "") {
+            $option_params["max_id"] = $max_id; // $max_idより古いTweetを取得
+        }
+
+        // TwitterAPI連携
+        $res = $this->Action("search/tweets", $option_params, "GET");
+        if (!isset($res["statuses"]) || !is_array($res["statuses"])) {
+            // Tweetを取得できなければ再試行
+            $request_count++;
+            return $this->SaveUniqueTweetImages($aryKeyCheck, $img_count, $max_id, $request_count);
+        }
+
+        foreach ($res["statuses"] as $status) {
+            // 10個の画像を取得した時点で終了
+            if ($img_count > 10) {
+                break;
+            }
+
+            $max_id = $status["id_str"];
+
+            // 画像がない || 同一画像ならスキップ
+            if (!isset($status['entities']['media'][0]["media_url_https"]) ||
+                isset($aryKeyCheck[$status['entities']['media'][0]["media_url_https"]])) {
+                    continue;
+                }
+
+                // 画像データの取得
+                $data = @file_get_contents($status['entities']['media'][0]["media_url_https"]);
+                if ($data !== false) {
+                    // パス付ファイル名作成
+                    $ext = substr($status['entities']['media'][0]["media_url_https"], strrpos($status['entities']['media'][0]["media_url_https"], '.') + 1);
+                    $file_path = DIR_TWEET_IMAGE . "tweet_img_{$this->now}_{$img_count}.{$ext}";
+
+                    // 画像の保存
+                    $result = @file_put_contents($file_path, $data);
+                    if ($result !== false) {
+                        // 保存に成功していればパスを出力
+                        echo $file_path , PHP_EOL;
+
+                        // 情報更新
+                        $aryKeyCheck[$status['entities']['media'][0]["media_url_https"]] = true;
+                        $img_count++;
+                    }
+                }
+        }
+
+        // 異なる画像を10個保存できれば終了。できていなければ再試行
+        if ($img_count > 10) {
+            return true;
+        } else {
+            $request_count++;
+            return $this->SaveUniqueTweetImages($aryKeyCheck, $img_count, $max_id, $request_count);
+        }
     }
 }
