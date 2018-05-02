d4f1c8765ba987e2d997dcafc919006a5fe96b69
no1s-goma <application.goto.2020@gmail.com>
2018-05-02 11:14:28 +0900

    [add]No1sページのウェブクローリング処理追加

--- lib/WebScrapingAction.php
+++ lib/WebScrapingAction.php
@@ -6,6 +6,10 @@ class WebScrapingAction
 {
     protected $html;
 
+    protected $aryUniqueCheck;
+    protected $baseUrlPattern;
+    protected $titlePattern = '/<title>[\n]*(.*?)[\n]*<\/title>/i';
+
     /**
      * コンストラクタ
      *
@@ -20,7 +24,7 @@ class WebScrapingAction
                 echo '指定のURLを読み込むことが出来ませんでした。' . PHP_EOL;
                 exit;
             } else {
-                $this->html = mb_convert_encoding($html, "utf-8", "sjis-win");
+                $this->html = mb_convert_encoding($html, "utf-8", "auto");
             }
         }
     }
@@ -51,5 +55,135 @@ class WebScrapingAction
     public function setHtml($html)
     {
         $this->html = mb_convert_encoding($html, "HTML-ENTITIES", "auto");
+    }
+
+    /**
+     * ウェブクローリング
+     * [TODO]重い。。処理方法要検討。
+     * titleタグ取得がネックで
+     *
+     * @param string $url
+     * @return boolean
+     */
+    public function WebCrawling($url)
+    {
+        // 初回のURLをベースと判断する
+        if (!isset($this->baseUrlPattern)) {
+            $this->baseUrlPattern = '/' . preg_quote($url, '/') . '/';
+        }
+
+        if (isset($this->aryUniqueCheck[$url]) || preg_match($this->baseUrlPattern, $url) === 0) {
+            return true;
+        }
+
+        // htmlデータを取得
+        $this->html = @file_get_contents($url);
+        if ($this->html === false) {
+            echo 'HTML取得エラー' , PHP_EOL;
+            return false;
+        } else {
+            $this->html = mb_convert_encoding($this->html, "utf-8", "auto");
+        }
+
+        // URLとページタイトルを出力
+        if (preg_match($this->titlePattern, $this->html, $title) === 1) {
+            echo $url, "\t", $title[1], PHP_EOL;
+
+            // URLユニークチェック変数にurlを追加
+            $this->aryUniqueCheck[$url] = true;
+        } else {
+            echo 'タイトル取得エラー' , PHP_EOL;
+            return false;
+        }
+
+        $elements = $this->getElementsByXpath('//a/@href');
+        foreach ($elements as $element) {
+            // href内のリンクを取得
+            $href = $element->nodeValue;
+            // 再起的に処理を実施
+            if (!$this->WebCrawling($href)) {
+                echo 'Webスクレイピングエラー' , PHP_EOL;
+                return false;
+            }
+        }
+
+        // 全て正常に処理されるとここにたどり着く
+        return true;
+    }
+
+    /**
+     * ウェブクローリング２
+     * 並列リクエスト処理
+     *
+     * [TODO]要調査500,503エラーが出たり、htmlが正しく取得できない場合がある
+     *
+     * @param mixed $urls 初回だけstring
+     * @return boolean
+     */
+    public function WebCrawling2($urls)
+    {
+        // 初回のURLをベースと判断する
+        if (!isset($this->baseUrlPattern)) {
+            $this->baseUrlPattern = '/' . preg_quote($urls, '/') . '/';
+            $urls = [$urls];
+        }
+
+        $curl_opts = [];
+        // $count_key = 0;
+        foreach ($urls as $count => $url) {
+            // 出力済みURL || 対象外URLの場合処理終了
+            if (isset($this->aryUniqueCheck[$url]) || preg_match($this->baseUrlPattern, $url) === 0) {
+                continue;
+            }
+
+            $this->aryUniqueCheck[$url] = true;
+
+                $curl_opts[] = [
+                        CURLOPT_URL            => $url,
+                        CURLOPT_PRIVATE        => $url,
+                        CURLOPT_SSL_VERIFYPEER => false,
+                        CURLOPT_SSL_VERIFYHOST => false,
+                        CURLOPT_RETURNTRANSFER => true,
+                        CURLOPT_TIMEOUT        => CURL_TIMEOUT,
+                        CURLOPT_CONNECTTIMEOUT => CURL_TIMEOUT,
+                        // リダイレクト対策
+                        CURLOPT_FOLLOWLOCATION => true,
+                        CURLOPT_MAXREDIRS      => 100,
+                        CURLOPT_AUTOREFERER    => true,
+                ];
+        }
+        if (empty($curl_opts)) {
+            return true;
+        }
+
+        $response = RequestApi::multiRequest($curl_opts);
+        if ($response["result"] === "ng") {
+            echo $response['response'] , PHP_EOL;
+            return false;
+        }
+
+        $concat_html = '';
+        foreach ($response['response'] as $url => $html) {
+            // [TODO]URLとページタイトルを出力
+            if (preg_match($this->titlePattern, $html, $title) === 1) {
+                echo $url, "\t", $title[1] . PHP_EOL;
+                $concat_html .= $html;
+            } else {
+                var_dump($url);
+                print_r($html);
+                echo 'タイトル取得エラー', PHP_EOL;
+                return false;
+            }
+        }
+        $this->html = $concat_html;
+
+        $hrefs = [];
+        $elements = $this->getElementsByXpath('//a/@href');
+        foreach ($elements as $element) {
+            // href内のリンクを取得
+            $hrefs[] = $element->nodeValue;
+        }
+
+        return $this->webCrawling($hrefs);
     }
 }
